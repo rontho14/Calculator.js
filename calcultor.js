a73953f781532150ce6a7eea6014d378397f2c2f
Guilherme Thomasi Ronca <gtronca@rd.com.br>
2025-10-14 13:56:50 -0300

    feat: adding event listerners to show numbers on calculator screen

--- calcultor.js
+++ calcultor.js
@@ -15,5 +15,26 @@ function divide(x, y) {
 }
 
 function operate(x, operator, y) {
-    
+    switch (operator) {
+        case "+":
+            return add(x, y);
+        case "-":
+            return subtract(x, y);
+        case "x": 
+            return multiply(x, y);
+        case "/":
+            return divide(x, y);
+    }
+}
+
+
+let content = document.querySelector(".result");
+content.innerText = "";
+let btn = document.querySelectorAll('.number-key');
+console.log(btn)
+
+for (let i =0 ; i < btn.length; i++) {
+    btn[i].addEventListener('click', ()=>{
+        content.innerText += btn[i].innerText;
+    })
 }
